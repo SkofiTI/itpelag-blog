6c34d4b96436aa63ac339cdb9b8fe113d9d2a6f7
SkofiTI <cactus.master994@gmail.com>
2024-04-25 12:31:00 +0300

    Создан класс Container. Реализованы все базовые методы контейнера.

---
+++ framework/src/Container/Container.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Framework\Container;
+
+use Framework\Container\Exceptions\ContainerException;
+use Psr\Container\ContainerInterface;
+
+class Container implements ContainerInterface
+{
+    private array $services = [];
+
+    public function add(string $id, string|object $concrete = null)
+    {
+        if (is_null($concrete)) {
+            if (!class_exists($id)) {
+                throw new ContainerException("Service $id not found");
+            }
+
+            $concrete = $id;
+        }
+        $this->services[$id] = $concrete;
+    }
+
+    public function get(string $id)
+    {
+        return new $this->services[$id];
+    }
+
+    public function has(string $id): bool
+    {
+        return array_key_exists($id, $this->services);
+    }
+}
